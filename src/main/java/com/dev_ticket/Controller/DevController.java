ea7dcf06614ac07a518021f78e81f6805351e1d0
Jubril <jubrilbucknor@gmail.com>
2023-09-15 17:17:25 +0100

    Manual Refactoring

--- src/main/java/com/dev_ticket/Controller/DevController.java
+++ src/main/java/com/dev_ticket/Controller/DevController.java
@@ -7,29 +7,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 //http://localhost:2506
 @RequiredArgsConstructor
-@RestController("/api/")
+@RestController
+@RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class DevController {
 
     private final DevService devService;
 
     @PostMapping("save-todo")
-    public ResponseEntity<Todo> addTodo(@RequestBody TodoDTO todoDTO) {
+    public ResponseEntity<?> addTodo(@RequestBody TodoDTO todoDTO) {
         Todo task = devService.saveTodo(todoDTO);
         return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
     @GetMapping("all-todo-items")
-    public ResponseEntity<List<Todo>> getAllTodoItems(TodoDTO todoDTO) {
+    public ResponseEntity<?> getAllTodoItems(TodoDTO todoDTO) {
         List<Todo> todos = devService.getAllTodos(todoDTO);
         return ResponseEntity.ok(todos);
     }
--- src/main/java/com/dev_ticket/Service/DevService.java
+++ src/main/java/com/dev_ticket/Service/DevService.java
@@ -4,6 +4,7 @@ import com.dev_ticket.DTO.TodoDTO;
 import com.dev_ticket.Model.Todo;
 import com.dev_ticket.Repository.DevRepo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +12,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class DevService {
-    private final DevRepo devRepo;
 
+    private final DevRepo devRepo;
     public List<Todo> getAllTodos(TodoDTO todoDTO) {
         return devRepo.findAll();
     }
     public Todo saveTodo(@RequestBody TodoDTO todoDTO) {
-        Todo todo = new Todo();
+        Todo todo = new Todo(todoDTO);
+        log.info("I'm here===>");
         return devRepo.save(todo);
     }
 }
